cbcb639caa9c5274b06c1b306ec32e36c9019c56
Vladar4 <vladar4@gmail.com>
2017-06-04 11:10:29 +0300

    added data.nim

---
+++ src/data.nim
@@ -0,0 +1,51 @@
+import
+  nimgame2 / [
+    assets,
+    audio,
+    font,
+    scene,
+    texturegraphic,
+    truetypefont,
+    types,
+  ]
+
+
+const
+  GameWidth* = 640
+  GameHeight* = 360
+  GameTitle* = "Nimgame 2 Platformer"
+
+
+var
+  titleScene*, mainScene*: Scene
+  defaultFont*, bigFont*: TrueTypeFont
+  gfxData*: Assets[TextureGraphic]
+  sfxData*: Assets[Sound]
+  score*: int
+
+
+proc loadData*() =
+  defaultFont = newTrueTypeFont()
+  if not defaultFont.load("data/fnt/FSEX300.ttf", 16):
+    echo "ERROR: Can't load font"
+  bigFont = newTrueTypeFont()
+  if not bigFont.load("data/fnt/FSEX300.ttf", 32):
+    echo "ERROR: Can't load font"
+
+  gfxData = newAssets[TextureGraphic](
+    "data/gfx",
+    proc(file: string): TextureGraphic = newTextureGraphic(file))
+
+  sfxData = newAssets[Sound](
+    "data/sfx",
+    proc(file: string): Sound = newSound(file))
+
+
+proc freeData*() =
+  defaultFont.free()
+  bigFont.free()
+  for graphic in gfxData.values:
+    graphic.free()
+  for sound in sfxData.values:
+    sound.free()
+
