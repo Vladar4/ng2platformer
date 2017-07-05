07cb4625538488534dc34bb9119fe6b3a4b9bb8d
Vladar4 <vladar4@gmail.com>
2017-07-05 12:52:35 +0300

    added enemy.nim

---
+++ src/enemy.nim
@@ -0,0 +1,57 @@
+import
+  nimgame2 / [
+    collider,
+    entity,
+    graphic,
+    texturegraphic,
+    tilemap,
+    types,
+  ],
+  data,
+  level
+
+
+const
+  GravAcc = 1000
+  WalkVel = 10
+
+
+type
+  Enemy* = ref object of Entity
+    level*: Level
+
+
+proc right*(enemy: Enemy) =
+  enemy.vel.x = WalkVel
+
+
+proc left*(enemy: Enemy) =
+  enemy.vel.x -= WalkVel
+
+
+proc enemyLogic(enemy: Entity, elapsed: float) =
+  discard #TODO
+
+
+proc init*(enemy: Enemy, graphic: TextureGraphic, level: Level) =
+  enemy.initEntity()
+  enemy.tags.add "enemy"
+  enemy.level = level
+  enemy.graphic = graphic
+  enemy.logic = enemyLogic
+
+  # collider
+  let c = newPolyCollider(enemy, points = [(0.0, 15.0), (15, 0), (31, 15)])
+  c.tags.add "level"
+  enemy.collider = c
+
+  # physics
+  enemy.acc.y = GravAcc
+  enemy.fastPhysics = true
+  enemy.physics = platformerPhysics
+
+
+proc newEnemy*(graphic: TextureGraphic, level: Level): Enemy =
+  new result
+  result.init(graphic, level)
+
